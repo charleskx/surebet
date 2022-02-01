270c38c1fd6c25e340de913404d09057dc438883
Charleston Amaral <charleston.silva03@gmail.com>
2022-02-01 18:10:31 -0300

    feat: CRUD users

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,13 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
 
-type Data = {
-  name: string
+export interface IData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<IData | IData[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  switch (req.method) {
+    case 'GET':
+      await prisma.users
+        .findMany()
+        .then((success) => res.status(200).json(success))
+        .catch((error) => res.status(405).json(error));
+      break;
+    case 'POST':
+      await prisma.users
+        .create({ data: req.body })
+        .then((success) => res.status(201).json(success))
+        .catch((error) => res.status(405).json(error));
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
---
+++ pages/api/users/[id].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+import { IData } from '../users';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IData | null>
+) {
+  const id = Number(req.query.id);
+
+  switch (req.method) {
+    case 'GET':
+      await prisma.users
+        .findUnique({ where: { id } })
+        .then((success) => res.status(200).json(success))
+        .catch((error) => res.status(405).json(error));
+      break;
+    case 'PUT':
+    case 'PATCH':
+      await prisma.users
+        .update({ where: { id }, data: { ...req.body } })
+        .then((success) => res.status(200).json(success))
+        .catch((error) => res.status(405).json(error));
+      break;
+    case 'DELETE':
+      await prisma.users
+        .delete({ where: { id } })
+        .then(() => res.status(204).end('record removed from database.'))
+        .catch((error) => res.status(405).json(error));
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'PUT', 'PATCH', 'DELETE']);
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
